1bd0ef367e166f08950da777d8d3466aec409135
XWasNotDeclared <122443962+XWasNotDeclared@users.noreply.github.com>
2024-05-19 00:07:42 +0700

    a

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 // Thiết lập Pug làm template engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
